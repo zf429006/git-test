2bd04adadc44ef4ebdba9ab2a382255e6852b21e
zf <1147071690@.com>
2019-05-09 19:56:42 +0800

    吧本地工程添加到git

---
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("想攻击我,先和我的影子玩拳击吧");
+        System.out.println("黑玫瑰,将再次绽放");
+        System.out.println("懦弱之举,绝不姑息");
+        System.out.println("小个子?你才是小个子");
+        System.out.println("提莫队长正在待命");
+        System.out.println("你看见过我的小熊吗");
+    }
+}
